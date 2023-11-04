86bde2e02c87e9f37b13e65a3fdc08b07ea221df
DevIA05 <dia05@polenumerique.re>
2023-11-04 13:13:40 +0400

    update

--- src/src/settings.py
+++ src/src/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', 'nlp-backend.azurewebsites.net']
+
+ALLOWED_HOSTS = ['0.0.0.0', 'nlp-backend.azurewebsites.net', 'localhost', '127.0.0.1']
 
 
 # Application definition
