d8a064add44557d5788a6db4a5fad42d6ac54dfd
DevIA05 <dia05@polenumerique.re>
2023-11-18 23:37:02 +0400

    ajout: connection + table

---
+++ src/src/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+class Film(models.Model):
+    titre = models.CharField(max_length=100)
+    note_spectateur = models.IntegerField()
+    note_presse = models.IntegerField()
+    nombre_commentaires = models.IntegerField()
+
+    def __str__(self):
+        return self.titre
+    
+    class Meta:
+        db_table = 'films'
+    
+class Commentaire(models.Model):
+    commentaire = models.TextField()
+    note = models.IntegerField()
+    note_like = models.IntegerField()
+    note_dislike = models.IntegerField()
+
+    def __str__(self):
+        return self.commentaire
+    
+    class Meta:
+        db_table = 'commentaire'
+    
+class NLP(models.Model):
+    model = models.CharField(max_length=255)
+    film = models.ForeignKey(Film, on_delete=models.CASCADE)
+    note = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.model} - {self.film.titre}"
+    
+    class Meta:
+        db_table = 'nlp'
