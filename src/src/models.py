d7dbf5a18b25f4b0a37cf4ed90780ed3fb3bc8fe
Terence0001 <111972075+Terence0001@users.noreply.github.com>
2023-11-25 22:21:54 +0400

    ajout titre_film dans la table Commentaire

--- src/src/models.py
+++ src/src/models.py
@@ -16,6 +16,8 @@ class Commentaire(models.Model):
     pseudo = models.TextField(null=True)
     commentaire = models.TextField()
     note = models.IntegerField()
+    titre_film = models.CharField(max_length=100, null=True)
+
 
     def __str__(self):
         return self.commentaire
