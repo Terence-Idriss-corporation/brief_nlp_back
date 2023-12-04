2820899e3e442053024292bd23d0c98a4db9045b
DevIA05 <111973830+DevIA05@users.noreply.github.com>
2023-12-04 11:09:58 +0400

    update path

--- src/front/views.py
+++ src/front/views.py
@@ -2,7 +2,9 @@ from django.http import HttpResponse
 from django.shortcuts import render
 import pandas as pd
 import pdb
+
 import os
+from django.conf import settings
 # Create your views here.
 
 def index(request):
@@ -10,7 +12,9 @@ def index(request):
     repertoire_de_travail = os.getcwd()
     print("Répertoire de travail actuel :", repertoire_de_travail)
     # pdb.set_trace() 
-    df = pd.read_csv("data/best_films.csv", sep=";")
+    file_path = os.path.join(settings.BASE_DIR, 'front/static/data/best_films.csv')
+    print("============= ", file_path)
+    df = pd.read_csv(file_path, sep=";")
     films_list = df.to_dict(orient='records')
     context = {'films_list': films_list}
     return render(request, "index.html", context)
